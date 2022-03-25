e45c3e0aa0d259b7f3de67bf9f4f0c43e0cc8fd5
mmausler <michaelmausler@gmail.com>
2022-03-25 09:24:24 -0400

    exercise 6

---
+++ exercises/exercise-6/js/main.js
@@ -0,0 +1,25 @@
+const fetchMovies = async () => {
+    const response = await fetch('https://api.airtable.com/v0/appxfJxfQb92yyKkg/Table%201?maxRecords=100&view=Grid%20view&api_key=keykxG25CNr82Rf9Y').then(data => data.json());
+
+    console.log(response);
+
+    const moviesContainer = document.getElementById('movies-container');
+
+    response.records.forEach(movie => {
+        console.log(movie.fields);
+        const articleEl = document.createElement('article');
+        const titleEl = document.createElement('div');
+        const genreEl = document.createElement('div');
+        const imdbUrlEl = document.createElement('div');
+        const releaseDateEl = document.createElement('div');
+
+        titleEl.innerHTML = movie.fields.Title;
+        genreEl.innerHTML = movie.fields.Genre;
+
+        articleEl.appendChild(titleEl);
+
+        moviesContainer.appendChild(articleEl);
+    });
+};
+
+fetchMovies();
